481d5e68e9fc1f3a50019baf91129c825b93e1a1
erasiega <gaseprogresando@hotmail.com>
2024-07-24 10:44:16 -0500

    1

---
+++ Ejercicio5/Ejercicio5/Ejercicio5/Pages/Tareas/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ejercicio5.Pages.Tareas
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Ejercicio5/Ejercicio5/Ejercicio5/Startup.cs
@@ -0,0 +1,10 @@
+﻿using Ejercicio5.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+public void ConfigureServices(IServiceCollection services)
+{
+    services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+    services.AddControllersWithViews();
+}
